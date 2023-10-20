b58756c8e576eb088b507fe13870b2bae3ec5d4a
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-10-21 02:26:05 +0800

    fix: opensea preview - move `/assets` into public folder - add dynamic import for App.tsx

--- src/components/ShareDialog/index.tsx
+++ src/components/ShareDialog/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import copy from 'assets/icons/copy.png'
+import copy from '/icons/copy.png'
 
 import {
   EmailShareButton,
