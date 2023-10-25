070451a813285324794e8bb57c1ca357a544dd8e
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-10-26 00:12:49 +0800

    add link in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ import { useAccount, useNetwork } from 'wagmi'
 import { useBoundStore } from 'store'
 import { CURRENT_CHAIN } from 'store/slices/wallet.slice'
 import logo from '/img/logo.png'
+import { InventoryIcon, MintIcon } from 'components/Icons/icons'
 
 export default function Header() {
   const { setCurrentWalletState, setWalletState } = useBoundStore()
@@ -24,15 +25,31 @@ export default function Header() {
 
   return (
     <Disclosure as="nav" className="bg-transparent">
-      <div className="mx-auto max-w-[3840px] py-5">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <Link to="/">
-                <img className="block h-10 w-auto lg:hidden" src={logo} alt="Nous Psyche" />
-                <img className="hidden h-20 w-auto lg:block" src={logo} alt="Nous Psyche" />
-              </Link>
-            </div>
+      <div className="mx-auto max-w-[3840px] py-1">
+        <div className="relative flex h-16 items-center justify-between px-3">
+          <div className="flex flex-shrink-0 items-center">
+            <Link to="/">
+              <img className="block h-10 w-auto lg:hidden" src={logo} alt="Nous Psyche" />
+              <img className="hidden h-16 w-auto lg:block" src={logo} alt="Nous Psyche" />
+            </Link>
+          </div>
+          <div className="flex gap-3 text-white">
+            <Link
+              to="/mint"
+              className="flex items-center gap-2 px-4 h-10 py-2 hover:bg-orange-200 hover:text-orange-800 rounded-lg"
+            >
+              <MintIcon /> Mint
+            </Link>
+            {address && (
+              <>
+                <Link
+                  to="/inventory"
+                  className="flex items-center gap-2 px-4 h-10 py-2 hover:bg-orange-200 hover:text-orange-800 rounded-lg"
+                >
+                  <InventoryIcon /> Inventory
+                </Link>
+              </>
+            )}
           </div>
           <ConnectButton
             chainStatus={'none'}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,8 +5,9 @@ import { Web3Wrapper } from 'App'
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Web3Wrapper>
+      <Header />
+      <hr className="h-px mb-8 bg-gray-700 border-0 dark:bg-gray-700" />
       <div className="container mx-auto text-white">
-        <Header />
         <Outlet />
       </div>
     </Web3Wrapper>
